8db542b5cee4307d33615c0b867a5c6a483568d0
max <maximoandreaq@gmail.com>
2021-01-30 16:37:06 -0600

    Matriz

---
+++ Scripts/EnemyGrid.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyGrid : MonoBehaviour
+{
+    public Sprite[] ComponentImages;
+    public string[] ComponentNames;
+    public int[] ComponentPrice;
+    public int[,] ComponentSize;
+
+    public GameObject[,] PlayerComponents;
+
+    public int cols = 15;
+    public int rows = 15;
+
+    public float tileSize = 1;
+
+    private Transform ComponentsContainer;
+
+    public static int selectedComponent = 0;
+    public static float[,] PlayerMatrix = new float[15, 15];
+    public static bool[,] PlayerSet = new bool[15, 15];
+
+    public static GameObject[,] matrix = new GameObject[15, 15];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComponentSize = new int[,] { { 1, 2 }, { 1, 1 }, { 1, 2 }, { 1, 2 }, { 2, 2 }, { 1, 2 } };
+
+        PlayerComponents = new GameObject[cols, rows];
+
+        ComponentsContainer = GameObject.Find("EnemyGrid").GetComponent<Transform>();
+
+        GenerateComponents();
+
+        GenerateMatrix();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void GenerateMatrix()
+    {
+        GameObject reference = (GameObject)Instantiate(Resources.Load("tile"));
+
+        for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = Instantiate(reference, transform);
+                Point point = tile.GetComponent<Point>();
+
+                float posX = col * tileSize;
+                float posY = row * tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+
+                PlayerComponents[col, row] = tile;
+                PlayerMatrix[col, row] = -1; // Esto es solo para indicar que no hay nada en la matriz
+                PlayerSet[col, row] = false; // Esto es solo para indicar que no hay nada en la matriz
+
+                int tmpCol = col;
+                int tmpRow = row;
+
+                point.setPoint(tmpCol, tmpRow);
+
+                matrix[col, row] = tile;
+            }
+
+        Destroy(reference);
+    }
+
+    // Genera la lista de componentes disponibles
+    private void GenerateComponents()
+    {
+        GameObject reference = (GameObject)Instantiate(Resources.Load("Component"));
+
+        for (int i = 0; i < ComponentImages.Length; i++)
+        {
+            GameObject component = Instantiate(reference, ComponentsContainer);
+
+            Text name = component.transform.Find("ComponentName").GetComponent<Text>();
+            Image image = component.transform.Find("ComponentImage").GetComponent<Image>();
+            Button button = component.transform.Find("ComponentButton").GetComponent<Button>();
+
+            name.text = ComponentNames[i];
+            image.sprite = ComponentImages[i];
+
+            int tmpIndex = i;
+
+            button.onClick.AddListener(delegate {
+                selectedComponent = tmpIndex;
+            });
+        }
+
+        Destroy(reference);
+    }
+
+
+
+}
